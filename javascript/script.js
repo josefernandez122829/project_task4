01d3d1e718742e34b06e5444f6d3a9271a1f7b80
josefernandez122829 <49940477+josefernandez122829@users.noreply.github.com>
2025-01-15 03:13:47 +0800

    practice-coding-email-password-validation

--- javascript/script.js
+++ javascript/script.js
@@ -6,11 +6,24 @@ $(function(){
         let email = $("#email").val();
         let password = $("#password").val();
         let cpassword = $("#cpassword").val();
+        let regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+        if(fname == '') {
+            alert('Please fill out First name field.');
+        }
+
+        if(lname == '') {
+            alert('Please fill out Last name field.');
+        }
+
+        if(!regex.test(email)) {
+            alert('Please match the requested format.');
+        }
 
         if(password === cpassword) {
             alert('true');
         }else {
-            alert('false');
+            alert('Password and Confirm password Not Match!');
         }
     });
 });
